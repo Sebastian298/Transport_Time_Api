fed6f9d5701dae93f4ab4d86ffcd077c05d764f0
Sebastian298 <jaimetenoriorios@outlook.com>
2024-11-23 17:26:12 -0600

    definicion e implementacion del servicio de dapper, funcion para ejecutar un query string y funcion para ejecutar un stored procedure

---
+++ Transport_Time/Services/DapperService.cs
@@ -0,0 +1,69 @@
+﻿using Dapper;
+using System.Data;
+using Transport_Time.Data;
+using Transport_Time.Models;
+
+namespace Transport_Time.Services
+{
+    public class DapperService : IDapperService
+    {
+        private readonly DapperContext _dapperContext;
+
+        public DapperService(DapperContext dapperContext)
+        {
+            _dapperContext = dapperContext;
+        }
+
+        public async Task<DapperServiceResponse> ExecuteQueryAsync<T>(string query, DynamicParameters? parameters = null)
+        {
+            try
+            {
+                using (IDbConnection connection = _dapperContext.CreateConnection())
+                {
+                    var results = await connection.QueryAsync<T>(query, parameters);
+                    return new DapperServiceResponse
+                    {
+                        HasError = false,
+                        Results = results
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                return new DapperServiceResponse
+                {
+                    HasError = true,
+                    InnerException = ex.Message
+                };
+
+            }
+        }
+
+        public async Task<DapperServiceResponse> ExecuteStoredProcedureAsync<T>(string storedProcedureName, DynamicParameters? parameters = null, bool hasArray = false)
+        {
+            try
+            {
+                using (IDbConnection connection = _dapperContext.CreateConnection())
+                {
+                    dynamic? response = hasArray
+                        ? await connection.QueryAsync<T>(storedProcedureName, parameters, commandType: CommandType.StoredProcedure)
+                        : await connection.QuerySingleOrDefaultAsync<T>(storedProcedureName, parameters, commandType: CommandType.StoredProcedure);
+
+                    return new DapperServiceResponse
+                    {
+                        HasError = false,
+                        Results = response
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                return new DapperServiceResponse
+                {
+                    HasError = true,
+                    InnerException = ex.Message
+                };
+            }
+        }
+    }
+}
---
+++ Transport_Time/Services/IDapperService.cs
@@ -0,0 +1,11 @@
+﻿using Dapper;
+using Transport_Time.Models;
+
+namespace Transport_Time.Services
+{
+    public interface IDapperService
+    {
+        Task<DapperServiceResponse> ExecuteQueryAsync<T>(string query, DynamicParameters? parameters = null);
+        Task<DapperServiceResponse> ExecuteStoredProcedureAsync<T>(string storedProcedureName, DynamicParameters? parameters = null, bool hasArray = false);
+    }
+}
