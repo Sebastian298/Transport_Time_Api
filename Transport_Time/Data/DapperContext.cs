d5ae4234b8c456305fb4dd0983a53bb04b297ad1
Sebastian298 <jaimetenoriorios@outlook.com>
2024-11-23 16:51:47 -0600

    creacion de dapper context

---
+++ Transport_Time/Data/DapperContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Transport_Time.Data
+{
+    public class DapperContext
+    {
+        private readonly string _connectionString;
+
+        public DapperContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
--- Transport_Time/Program.cs
+++ Transport_Time/Program.cs
@@ -1,7 +1,10 @@
+using Transport_Time.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!;
+builder.Services.AddSingleton<DapperContext>(provider => new DapperContext(connectionString!));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
