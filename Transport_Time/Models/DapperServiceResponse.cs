c5fbf53818884018a00bc34c63b64563c753cb0f
Sebastian298 <jaimetenoriorios@outlook.com>
2024-11-23 17:25:43 -0600

    Creacion de modelo para estandarizar las respuestas del servicio de dapper

---
+++ Transport_Time/Models/DapperServiceResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Transport_Time.Models
+{
+    public class DapperServiceResponse
+    {
+        public bool HasError { get; set; }
+        public string? InnerException { get; set; }
+        public dynamic? Results { get; set; }
+    }
+}
