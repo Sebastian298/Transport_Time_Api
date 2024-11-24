de8ab3a84c59ad06c7588481fcb56c65202d5911
Sebastian298 <jaimetenoriorios@outlook.com>
2024-11-24 17:33:27 -0600

    creacion de modelos para mapear el resultado del web service de ruteo de tom tom

---
+++ Transport_Time/Models/RoutingInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Transport_Time.Models
+{
+    public class RoutingInfo
+    {
+        public required Summary Summary { get; set; }
+        public required List<Points> Points { get; set; }
+    }
+}
---
+++ Transport_Time/Models/RoutingResponse.cs
@@ -0,0 +1,34 @@
+﻿namespace Transport_Time.Models
+{
+    public class RoutingResponse
+    {
+        public required List<Routes> Routes { get; set; }
+    }
+
+    public class Routes
+    {
+        public required Summary Summary { get; set; }
+        public required List<Legs> Legs { get; set; }
+    }
+
+    public class Legs
+    {
+        public required List <Points> Points { get; set; }
+    }
+
+    public class Summary
+    {
+        public float LengthInMeters { get; set; }
+        public int TravelTimeInSeconds { get; set; }
+        public int TrafficDelayInSeconds { get; set; }
+        public int TrafficLengthInMeters { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
+    }
+
+    public class Points
+    {
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+    }
+}
