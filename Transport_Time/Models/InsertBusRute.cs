f1bcebf60346ade49ef04170256e3b7c127d6106
Sebastian298 <jaimetenoriorios@outlook.com>
2024-11-23 22:50:14 -0600

    creacion de modelo para definir los datos que se recibiran en el endpoint para asignar una ruta para un truck

---
+++ Transport_Time/Models/InsertBusRute.cs
@@ -0,0 +1,8 @@
+﻿namespace Transport_Time.Models
+{
+    public class InsertBusRute
+    {
+        public required string BusId { get; set; }
+        public required string RuteId { get; set; }
+    }
+}
