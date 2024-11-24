831516589cf8f7670fc06a38c4234666bd0ef358
Sebastian298 <jaimetenoriorios@outlook.com>
2024-11-24 12:56:00 -0600

    creacion de modelo para estandarizar las respuestas hacia servicios rest

---
+++ Transport_Time/Models/HttpServiceResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Transport_Time.Models
+{
+    public class HttpServiceResponse<T>
+    {
+        public bool Success { get; set; }
+        public T? Content { get; set; }
+        public string? ErrorMessage { get; set; }
+    }
+}
