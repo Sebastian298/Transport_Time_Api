9ac45b0a26b0af6ae9068189213acba1bdd6724f
Sebastian298 <jaimetenoriorios@outlook.com>
2024-11-23 22:49:46 -0600

    creacion de modelo para estandarizar las respuestas de los stored procedures que ejecuten una sentencia insert/update

---
+++ Transport_Time/Models/CrudResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Transport_Time.Models
+{
+    public class CrudResponse
+    {
+        public bool Success { get; set; }
+        public required string Message { get; set; }
+        public string? InnerException { get; set; }
+    }
+}
