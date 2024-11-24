2412da3e9c2957b686ea722e8ae8cf949dcd8d31
Sebastian298 <jaimetenoriorios@outlook.com>
2024-11-23 20:37:43 -0600

    definicion de interfaz ITransport que nos servira para definir la logica de la aplicacion que se requiera en el backend

---
+++ Transport_Time/Repositories/ITransportService.cs
@@ -0,0 +1,10 @@
+﻿using Transport_Time.Models;
+
+namespace Transport_Time.Repositories
+{
+    public interface ITransportService
+    {
+        Task<IEnumerable<ModelToDropdown>> GetUnnasignedTrucks();
+        Task<IEnumerable<ModelToDropdown>> GetUnnasignedRoutes();
+    }
+}
