b92c81e9a8e7e165cb59505090012d4bcef05299
Sebastian298 <jaimetenoriorios@outlook.com>
2024-11-23 21:45:42 -0600

    creacion de modelo para estandarizar una estructura de respuesta en la api

---
+++ Transport_Time/Models/GenericResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Transport_Time.Models
+{
+    public class GenericResponse<T>
+    {
+        public int StatusCode { get; set; }
+        public T? Content { get; set; }
+        public string? InnerException { get; set; }
+    }
+}
