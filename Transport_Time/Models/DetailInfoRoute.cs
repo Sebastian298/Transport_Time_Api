5fc26a11ed88ae6378fba4dd86c476acdb36d523
Sebastian298 <jaimetenoriorios@outlook.com>
2024-11-23 23:51:19 -0600

    creacion de modelo para obtener el resultado del stored procedure que nos trae el detalle de la ruta con el truck

---
+++ Transport_Time/Models/DetailInfoRoute.cs
@@ -0,0 +1,9 @@
+﻿namespace Transport_Time.Models
+{
+    public class DetailInfoRoute
+    {
+        public required string RouteName { get; set; }
+        public required string TruckName { get; set; }
+        public required string InitialLocation { get; set; }
+    }
+}
